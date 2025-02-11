8443887174dd9c5f8936dea1d68aa718cc7776d5
luisgiraldo <luisgiraldo@titamedia.com>
2025-02-11 14:45:10 -0500

    Ajustes server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
-// server/server.js
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import routes from './routes/index.js'; // Importa las rutas principales
+import routes from './routes/index.js';
 require('dotenv').config(); 
 
-// Para obtener __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3200;
 
-// Sirve archivos estáticos (bundle.js, styles.css, etc.)
+// Sirve archivos estáticos (bundle.js, styles.css)
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Monta las rutas principales (SSR y API)
